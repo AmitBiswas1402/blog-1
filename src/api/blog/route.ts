52c719a24202829228a8b7b917702410d4c79fc6
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-05-06 16:33:34 +0530

    fix: Refactor database connection handling and improve GET response message

--- src/api/blog/route.ts
+++ src/api/blog/route.ts
@@ -1,13 +1,18 @@
 import { connectDB } from "@/lib/db/db";
 import { NextRequest, NextResponse } from "next/server";
 
-const db = async() => {
-    await connectDB();
-}
-
-db();
+connectDB().catch((err) => {
+  console.error("Database connection failed:", err);
+});
 
 export async function GET(){
-  return NextResponse.json({ msg: "API Working" });
+  return NextResponse.json({ message: "API Working" });
 }
 
+export async function POST(request: NextRequest) {
+  const formData = await request.formData();
+  const timestamp = Date.now();
+
+  const image = formData.get('image')
+  const imgByData = await image.arrayBuffer();
+}
